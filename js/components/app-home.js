c20a254de005a74aaf85bdf87c06a8bee930057a
solofo <solofo.ralitera@gmail.com>
2018-06-14 17:38:57 +0300

    Login interface

--- js/components/app-home.js
+++ js/components/app-home.js
@@ -1,6 +1,11 @@
 const R_HOME = Vue.component('app-home', {
     template: `
-        <section class="app-home bg-norepeat flex_h-center flex_v-center">
+        <section class="app-home bg-norepeat flex_h-center flex_v-center flex-column">
+            <article class="h-align-center font15 padding_1 margin_1 primary-font">
+                Audience interaction,
+                realtime polling and question 
+                platform from conferences
+            </article>            
             <div class="surface padding_1_0">
                 <app-join></app-join>
                 <footer class="background flex_v-center flex_h-center">
